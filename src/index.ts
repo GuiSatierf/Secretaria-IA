95cbd749d259a9e7b7d272907796b5e9b5875f83
GuiSatierf <guifreitas343@gmail.com>
2024-07-25 00:32:46 -0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,6 @@ import { initializeNewAIChatSession, mainOpenAI } from './service/openai';
 import { splitMessages, sendMessagesWithDelay } from './util';
 import { mainGoogle } from './service/google';
 
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-console.log(__dirname);
-
 dotenv.config();
 type AIOption = 'GPT' | 'GEMINI';
 
